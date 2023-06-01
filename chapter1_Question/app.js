7b93fa08534c2188fcfdea892a3d30f5752c276b
Pradep bhardwaj <pr275202@gmail.comgit config --global user.name Pradeep>
2023-06-01 11:25:31 +0530

    First commit

---
+++ chapter1_Question/app.js
@@ -0,0 +1,35 @@
+// chapter1 Question1
+
+let a = "harry"
+let b = 5
+document.write(a+b)
+  
+
+
+console.log(typeof (a+b))
+// chapter1 Question2
+
+const a1 = {
+    name:"harry",
+    section: 1,
+    isprinciple:false
+}
+console.log(a1)
+
+
+// chapter1 Question3
+
+a1['freind'] = "subham"
+a1['name'] = "lovish"
+
+console.log(a1)
+
+//chapter1 Question4
+
+const dict = {
+    appreciate: "to understand a problem, etc.", 
+    clarsach: "an ancient Irish and Scottish harp.",
+    prolegomenon: "noun. a preliminary discussion",
+}
+
+console.log("appreciate")
