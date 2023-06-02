c5df77ff34135764be7e732d52b1035a485a6221
Pradep bhardwaj <pr275202@gmail.comgit config --global user.name Pradeep>
2023-06-02 12:04:48 +0530

    you tube chapter 5 added

---
+++ you_tube chapter6/operaters/app.js
@@ -0,0 +1,45 @@
+// arthmatic operaters
+
+let a = 6
+let b = 44
+console.log ("a + b = ", a+b)
+console.log("a - b = ", a-b)
+console.log("a/b =", a/b)
+console.log("a*b = ", a*b)
+console.log("a**b = ",a**b)
+console.log("a%b =",a%b)
+console.log("++a = ",++a)
+console.log( "a++= ",a++)
+console.log("--a = ",--a)
+console.log("a-- = ",a--)
+console.log("a = ",a)
+
+console.log("a/b =", a/b)
+
+// assigenment operaters
+let assigenment = 1;
+assigenment +=5 //same ios assigment = assigement + 5
+console.log ("a is now = , a")
+assigenment -= 5 // same is assigement  -5
+console.log(" a is now = ",a)
+assigenment /=5 //same is assigment = assigement /5
+console.log( " a is now = ,a")
+
+// compparison operater
+
+let comp1 = 6;
+let comp2 =7;
+console.log("comp == comp2 is",comp1 == comp2)
+console.log("comp1 != comp2 is ", comp1 != comp2)
+console.log (" comp1 === comp2 is ", comp1 ===comp2)
+console. log ("comp1 !== comp2 ",comp1 = comp2)
+console.log("comp1 > comp2", comp1 > comp2)
+
+// logical operaters
+
+let x = 5
+let y =  6
+console.log( x<y && x==5)
+console.log( x>y || x==5)
+console.log(! false)
+console.log(!true)
