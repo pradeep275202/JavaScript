d660ba2dab42fc907f6da0eaae616e4b7e8e057c
Pradep bhardwaj <pr275202@gmail.comgit config --global user.name Pradeep>
2023-06-05 15:03:42 +0530

    loop and array file added

---
+++ loop and array/loop/app.js
@@ -0,0 +1,19 @@
+for (let x = 0; x<100 ; x++){
+    document.write("pradeep");
+    document.write("<br>");
+    document.write("<br>");
+}
+
+for (let z = 0; z<=10000 ; z++){
+    document.write("123123123")
+    document.write("<br>")
+    document.write("<br>")
+
+}
+
+for( let x = 0; x<100; x++){
+    console.log("addres")
+    console.log("br")
+    console.log("br")
+}
+
