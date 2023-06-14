a4e7350b1ad0ac1d265608d728ef2211c35b2a86
Pradep bhardwaj <pr275202@gmail.comgit config --global user.name Pradeep>
2023-06-14 12:59:57 +0530

    string methods

---
+++ string methods/practice_on_string/app.js
@@ -0,0 +1,42 @@
+//question no 1
+
+let fr = "Raman"+"Shivika"+"harry"
+console.log(fr[0]);
+console.log(fr[1]);
+console.log(fr[2]);
+console.log(fr[3]);
+
+console.log(fr)
+
+let str = "Rama\""
+console.log(str.length)
+
+//question no 2
+
+const sentence ='the quick brown fox jumps over the Lazy dog'
+const word ='fox'
+console.log(sentence.includes(word))
+
+//question no 3
+
+let upper ="indrajeet"
+console.log(upper.toLowerCase())
+console.log(upper.toUpperCase())
+
+
+//question no 4
+
+
+let str1 ="please give me Rs 10000"
+let amount = str1.slice(13)
+
+//let amount = str1.slice("please give Rs".length)
+
+//question no 5
+
+let freaind = "Deepika"
+freaind[3]="R"
+console.log(freaind)
+
+
+
